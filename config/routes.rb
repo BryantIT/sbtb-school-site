82c1d0805520a364562946139d10c35056bc1b88
Bryant Richards <dragonseyeimages@gmail.com>
2020-06-02 13:36:53 -0500

    name spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :years
   namespace :api do
     namespace :v1 do
+      resources :years
       resources :principals
       resources :parents
       resources :students
