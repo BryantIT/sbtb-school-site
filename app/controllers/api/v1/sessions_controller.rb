22909f6156bfeac5b784b5b540e906adb1a4d5d0
Bryant Richards <dragonseyeimages@gmail.com>
2020-06-04 13:01:48 -0500

    sessions

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user)
+    else
+      render json: {
+        error: "Email or Password does not exist"
+      }
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: UserSerializer.new(current_user)
+    else
+      render json: {
+        error: "No one currently logged in"
+      }
+    end
+  end
+
+  def destroy
+    session.clear
+    render json: {
+      notice: "Successfully logged out"
+    }
+  end
+end
