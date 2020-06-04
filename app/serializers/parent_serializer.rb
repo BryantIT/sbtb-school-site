dda16da46fe2368fcc7b7d32996fa1bef435d50d
Bryant Richards <dragonseyeimages@gmail.com>
2020-06-04 09:33:50 -0500

    updated serializer

--- app/serializers/parent_serializer.rb
+++ app/serializers/parent_serializer.rb
@@ -2,4 +2,5 @@ class ParentSerializer
   include FastJsonapi::ObjectSerializer
   attributes :first_name, :last_name, :middle_initial, :email, :about,
    :student_id
+   has_many :students, serializer: StudentSerializer
 end
