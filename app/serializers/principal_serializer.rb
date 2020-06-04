828b7725f2a8ecf09239f0670363a398aafc2fbf
Bryant Richards <dragonseyeimages@gmail.com>
2020-06-04 09:34:53 -0500

    updated serializer

--- app/serializers/principal_serializer.rb
+++ app/serializers/principal_serializer.rb
@@ -1,4 +1,5 @@
 class PrincipalSerializer
   include FastJsonapi::ObjectSerializer
   attributes :first_name, :last_name, :middle_initial, :email, :about, :role
+  has_many :teachers, serializer: TeacherSerializer
 end
